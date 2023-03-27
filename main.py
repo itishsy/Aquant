956a8b28dd5fb9f88885e36f7518f91871ab9647
itishsy <itishsy@163.com>
2023-03-27 21:54:38 +0800

    upset指定的code

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
-from storage import fetch_data
+from storage import fetch_data,upset
 from strategy.macd import search
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     fetch_data()
     stocks = ['510300', '512690', '512880']
+    for code in stocks:
+        upset(code)
     search(stocks=stocks, klt=102)
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
