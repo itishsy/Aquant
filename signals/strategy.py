027363bb230d932c50d08db0d026ff364437e492
itishsy <itishsy@163.com>
2023-05-14 17:05:03 +0800

    bottom

--- signals/strategy.py
+++ signals/strategy.py
@@ -69,9 +69,12 @@ def reverse_signals(candles: List[Candle]) -> List[Signal]:
         c_1 = mark_candles[i - 1]
         c_0 = mark_candles[i]
         if c_2.mark == -3 and c_1.mark == 3 and c_0.mark == -3 and c_2.diff() < 0 and c_1.diff() < 0 and c_0.diff() < 0:
-            low2 = get_lowest(candles, c_2.dt)
-            low0 = get_lowest(candles, c_0.dt)
-            if c_2.diff() < c_0.diff() and low2 > low0:
+            down_stage1 = get_stage(candles, c_2.dt)
+            down_stage2 = get_stage(candles, c_0.dt)
+
+            low1 = get_lowest(down_stage1)
+            low2 = get_lowest(down_stage2)
+            if c_2.diff() < c_0.diff() and low1 > low2:
                 signals.append(Signal(dt=c_0.dt, type='reverse', value=c_0.mark))
         if c_2.mark == 3 and c_1.mark == -3 and c_0.mark == 3 and c_2.diff() > 0 and c_1.diff() > 0 and c_0.diff() > 0:
             high2 = get_highest(candles, c_2.dt)
@@ -81,57 +84,23 @@ def reverse_signals(candles: List[Candle]) -> List[Signal]:
     return signals
 
 
-def get_lowest(candles: List[Candle], dt):
-    i = 0
-    s = len(candles)
-    lowest = 0
-    while i < s:
-        if candles[i].dt == dt:
+def get_lowest(candles: List[Candle]):
+    lowest = candles[0].low
+    i = 1
+    while i < len(candles):
+        if candles[i].low < lowest:
             lowest = candles[i].low
-            j = i - 1
-            k = i + 1
-            while j > 0:
-                if candles[j].mark > 0:
-                    break
-                if candles[j].low < lowest:
-                    lowest = candles[j].low
-                j = j - 1
-            while k < s:
-                if candles[k].mark > 0:
-                    break
-                if candles[k].low < lowest:
-                    lowest = candles[k].low
-                k = k + 1
-            break
-        else:
-            i = i + 1
+        i = i + 1
     return lowest
 
 
 def get_highest(candles: List[Candle], dt):
-    i = 0
-    s = len(candles)
-    highest = 0
-    while i < s:
-        if candles[i].dt == dt:
+    highest = candles[0].high
+    i = 1
+    while i < len(candles):
+        if candles[i].high > highest:
             highest = candles[i].high
-            j = i - 1
-            k = i + 1
-            while j > 0:
-                if candles[j].mark < 0:
-                    break
-                if candles[j].high > highest:
-                    highest = candles[j].high
-                j = j - 1
-            while k < s:
-                if candles[k].mark < 0:
-                    break
-                if candles[k].high > highest:
-                    highest = candles[k].high
-                k = k + 1
-            break
-        else:
-            i = i + 1
+        i = i + 1
     return highest
 
 
@@ -146,7 +115,7 @@ def get_stage(candles: List[Candle], dt) -> List[Candle]:
             k = i + 1
             while j > 0:
                 if (candles[j].mark > 0) == (candles[i].mark > 0):
-                    stage.insert(0,candles[j])
+                    stage.insert(0, candles[j])
                 else:
                     break
                 j = j - 1
@@ -161,21 +130,15 @@ def get_stage(candles: List[Candle], dt) -> List[Candle]:
     return stage
 
 
-def is_stage(candles: List[Candle], dt):
-
-    pass
-
-
-def is_trend(candles: List[Candle], dt):
-    pass
-
-
-c1 = find_candles('300239',101,'2023-04-01')
-s1 = get_stage(c1,'2023-04-17')
-lowest = s1[0].low
-i = 1
-while i < len(s1):
-    if s1[i].low < lowest:
-        lowest = s1[i].low
-    i = i + 1
-print(lowest)
+def is_trend(candles: List[Candle]):
+    """
+    无趋势不背离
+    :param candles:
+    :return:
+    """
+    if len(candles)< 3:
+        return False
+    i = 2
+    while i < len(candles):
+        i = i + 1
+    return False
