98bf1a196493df58b4634e13b09d4c540c076cce
itishsy <itishsy@163.com>
2023-06-05 18:18:12 +0800

    commit

--- strategies/watcher.py
+++ strategies/watcher.py
@@ -4,11 +4,12 @@ from storage.fetcher import fetch_and_save
 from storage.dba import find_candles
 from models.ticket import Ticket
 from models.trade import Trade
-from common.utils import dt_format, is_deal_time
+from common.utils import dt_format
 import traceback
 import time
 
 flag = False
+
 
 def freq_collect(ti: Ticket):
     fqs = []
@@ -23,10 +24,44 @@ def freq_collect(ti: Ticket):
     return fqs
 
 
+def is_watch_time(freq):
+    try:
+        freq = int(freq)
+    except:
+        print(freq)
+
+    now = datetime.now()
+    hm = now.hour * 100 + now.minute
+    if now.weekday() < 5:
+        if freq == 5 and hm in [946, 1006, 1016, 1036, 1056,
+                                1106, 1116, 1136, 1316, 1331, 1346,
+                                1406, 1421, 1436, 1451]:
+            return True
+        if freq == 15 and hm in [1001, 1031, 1101, 1131,
+                                 1331, 1401, 1431, 1501]:
+            return True
+        if freq in [30, 60] and hm in [1031, 1131, 1401, 1451]:
+            return True
+        if freq == 120 and hm in [1125, 1450, 1510]:
+            return True
+        if freq == 201 and hm in [1450, 1510]:
+            return True
+    return False
+
+
+def is_trade_time():
+    now = datetime.now()
+    if now.weekday() < 5:
+        return False
+    else:
+        hm = now.hour * 100 + now.minute
+        return 930 < hm < 1530
+
+
 def deal(ti: Ticket):
     fqs = freq_collect(ti)
     for fq in fqs:
-        if flag or is_deal_time(fq):
+        if flag or is_watch_time(fq):
             fetch_and_save(ti.code, fq)
             bfs = ti.buy.split(',')
             sfs = ti.sell.split(',')
@@ -36,14 +71,20 @@ def deal(ti: Ticket):
             ldt = dt_format(cds[-1].dt)
             if bfs.__contains__(fq):
                 sis = diver_bottom(cds)
-                print('deal code:{} buy:{},size:{}'.format(ti.code,fq,len(sis)))
-                if len(sis) > 0: # and sis[-1].dt >= ldt:
-                    Trade.create(code=ti.code, name=ti.name, freq=fq, dt=sis[-1].dt, type=0, price=sis[-1].value)
+                print('deal code:{} buy:{},size:{}'.format(ti.code, fq, len(sis)))
+                if len(sis) > 0:  # and sis[-1].dt >= ldt:
+                    si = sis[-1]
+                    if not Trade.select().where(Trade.code == si.code, Trade.dt == si.dt, Trade.freq == fq,
+                                                Trade.type == 0).exists():
+                        Trade.create(code=ti.code, name=ti.name, freq=fq, dt=si.dt, type=0, price=sis[-1].value)
             if sfs.__contains__(fq):
                 sis = diver_top(cds)
-                print('deal code:{} sell:{},size:{}'.format(ti.code,fq,len(sis)))
-                if len(sis) > 0: # and sis[-1].dt >= ldt:
-                    Trade.create(code=ti.code, name=ti.name, freq=fq, dt=sis[-1].dt, type=1, price=sis[-1].value)
+                print('deal code:{} sell:{},size:{}'.format(ti.code, fq, len(sis)))
+                if len(sis) > 0:  # and sis[-1].dt >= ldt:
+                    si = sis[-1]
+                    if not Trade.select().where(Trade.code == si.code, Trade.dt == si.dt, Trade.freq == fq,
+                                                Trade.type == 1).exists():
+                        Trade.create(code=ti.code, name=ti.name, freq=fq, dt=sis[-1].dt, type=1, price=sis[-1].value)
 
 
 def watch_all():
@@ -63,14 +104,14 @@ def daily_watch():
     while True:
         now = datetime.now()
         try:
-            if now.weekday() < 5:
+            if is_trade_time():
                 watch_all()
         except Exception as e:
             print(e)
         finally:
             if now.minute == 1:
-                print('[{}] searcher working ...'.format(now))
-            time.sleep(60)
+                print('[{}] watcher working ...'.format(now))
+            time.sleep(30)
 
 
 if __name__ == '__main__':
