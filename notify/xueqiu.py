4b30054a7cc87d9ad211f6b0747344e809f396ee
itishsy <itishsy@163.com>
2024-01-05 22:54:32 +0800

    flow

--- notify/xueqiu.py
+++ notify/xueqiu.py
@@ -1,8 +1,19 @@
 from components.rpa import *
 import logging
 
+
 def find_hot():
     access("https://xueqiu.com/hq")
-    el = WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, "style_hot-stock-list_3Ty")))
     ele = driver.find_element(By.CLASS_NAME, "style_hot-stock-list_3Ty")
-    ele.find_elements()
+    els = ele.find_elements(By.TAG_NAME, "a")
+    for el in els:
+        href = el.get_attribute("href")
+        code = href.split("/S/")[1]
+        if code.startswith("SH"):
+            print(code.replace("SH", ''))
+        elif code.startswith("SZ"):
+            print(code.replace("SZ", ''))
+
+
+if __name__ == '__main__':
+    find_hot()
