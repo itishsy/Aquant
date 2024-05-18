0191cb099317836d9dd345bf4b0017a29514ff2e
itishsy <itishsy@163.com>
2024-05-18 23:20:20 +0800

    d163

--- engines/d163.py
+++ engines/d163.py
@@ -17,10 +17,15 @@ class D163(Engine):
         dbs = diver_bottom(candles)
         if len(dbs) > 0:
             sig = dbs[-1]
-            sub_candles = find_candles(code, freq=30, begin=sig.dt)
+            sub_candles = find_candles(code, freq=60, begin=sig.dt)
             sub_abs = diver_bottom(sub_candles)
             if len(sub_abs) > 0:
                 return sub_abs[-1]
+            else:
+                sub_candles = find_candles(code, freq=30, begin=sig.dt)
+                sub_abs = diver_bottom(sub_candles)
+                if len(sub_abs) > 0:
+                    return sub_abs[-1]
 
     def watch(self, cho):
         pass
