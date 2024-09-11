bf345a12a2225647966da46a17d28e91cdbb0c8a
itishsy <itishsy@163.com>
2024-09-11 22:51:42 +0800

    common filter

--- jira_tj.py
+++ jira_tj.py
@@ -147,7 +147,7 @@ def up_all_story_status(version):
         up_story_status(story_key)
 
 
-def add_sub_task(parent_key, task_summary, task_description, task_assignee, task_component_id, task_priority=None, customfieldKeys=None, customfieldValues=None):
+def add_sub_task(parent_key, task_summary, task_description, task_assignee, task_component_id, fixVersions=None, task_priority=None, customfieldKeys=None, customfieldValues=None):
     issue_dict = {
         'project': {'key': 'SEEBOT'},
         'parent': {'key': parent_key},  # 指定父故事的问题键
@@ -157,6 +157,8 @@ def add_sub_task(parent_key, task_summary, task_description, task_assignee, task
         "assignee": {"name": task_assignee},  # 指定经办人用户名
         "components": [{"id": task_component_id}]  # 指定目标模块ID
     }
+    if fixVersions:
+        issue_dict['fixVersions'] = [{"name": fixVersions}]  # 指定修复的版本
 
     if task_priority:
         issue_dict['priority'] = {"name": task_priority}  # 指定优先级
@@ -173,7 +175,7 @@ def add_sub_task(parent_key, task_summary, task_description, task_assignee, task
 
     new_issue = jira.create_issue(fields=issue_dict)
     if new_issue:
-        print("子任务成功创建。")
+        print("子任务成功创建。", task_summary, new_issue.permalink())
 
 
 def add_link_Test(story_key, task_summary, task_description, task_assignee, task_component_id, customfieldKeys=None, customfieldValues=None):
@@ -261,6 +263,15 @@ def find_story_no_task(version):
             print(story.permalink())
 
 
+def find_issues(project='SEEBOT', version=""):
+    issue_arr = jira.search_issues('project = "SEEBOT" AND fixVersion="{}"'.format(version), maxResults=1000)
+    issues = []
+    for issue_key in issue_arr:
+        issue = jira.issue(id=issue_key, expand='changelog')
+        issues.append(issue)
+    return issues
+
+
 def find_un_close_story(version=""):
     print('版本【{}】开发及测试任务已完成，故事未关闭'.format(version))
     story_arr = jira.search_issues(
@@ -295,26 +306,238 @@ def find_un_close_story(version=""):
 
 
 if __name__ == '__main__':
+    version = 'BOT20240910,RPA20240910'
+    users = {'chaodongyue':"巢东岳",
+             'chenjingchun':"陈静纯",
+             'fengweilong':'冯尾龙',
+             'huangshouyi':'黄首益',
+             'liangheyun':'梁鹤云',
+             'lirongwei':'李荣威',
+             'lisongyang':'李松洋',
+             'liudandan':'刘丹丹',
+             'liuhongchang':'刘洪昌',
+             'liwensha':'李文莎',
+             'lixiaolong':'李小龙',
+             'pengshasha':'彭莎莎',
+             'tanyong':'谭咏',
+             'wangying':'王英',
+             'weizhaoxin':'韦兆新',
+             'xufuzhou':'徐富周',
+             'yanglianxin':'杨廉新',
+             'zengyixin':'曾轶鑫',
+             'zhangkailun':'张凯伦',
+             'zhengjiajie':'郑佳杰',
+             'zhenshijun':'甄仕军'}
 
-    version = 'BOT20230707'
+    usernames = users.keys()
+    print("版本：", version)
+    total = 0
+    done_size = 0
+    add_size = 0
+    delay_size = 0
+    InView_size = 0
 
+    for user in usernames:
+        # 版本统计
+        issues = jira.search_issues('fixVersion in ({}) AND assignee in ({}) '.format(version, user), maxResults=1000, expand='changelog')
+
+        done_arr = []
+        add_arr = []
+        delay_arr = []
+        InView_arr = []
+        task_arr = []
+        bug_arr = []
+        for issue in issues:
+            if issue.fields.issuetype.name == '故障':
+                bug_arr.append(issue)
+            elif issue.fields.issuetype.name in ['任务', 'Test', '故事', '子任务']:
+                task_arr.append(issue)
+
+            if issue.get_field('created') > '2024-09-03':
+                add_arr.append(issue)
+
+            status = issue.get_field('status').name
+            if status in ['待办', '处理中', '开放']:
+                delay_arr.append(issue)
+            elif status in ['完成', '已完成', '关闭']:
+                done_arr.append(issue)
+
+        if user in ['liwensha', 'lixiaolong', 'chenjingchun', 'yanglianxin']:
+            bugs = jira.search_issues('fixVersion in ({}) AND reporter in ({}) '.format(version, user),
+                                        maxResults=1000, expand='changelog')
+            for bug in bugs:
+                bug_arr.append(bug)
+
+                if bug.get_field('created') > '2024-09-03':
+                    add_arr.append(bug)
+
+                if bug.get_field('status').name == 'In View':
+                    InView_arr.append(bug)
+                elif bug.get_field('status').name in ['完成', '已完成', '关闭']:
+                    done_arr.append(bug)
+
+        if len(issues) > 0:
+            total = total + len(issues)
+            done_size = done_size + len(add_arr)
+            add_size = add_size + len(add_arr)
+            delay_size = delay_size + len(delay_arr)
+            InView_size = InView_size + len(InView_arr)
+
+            print(users.get(user), "任务：", "{}+{}".format(len(task_arr), len(bug_arr)), "完成：", len(done_arr), "插入：", len(add_arr), "延迟：", "{}+{}".format(len(delay_arr), len(InView_arr)))
+            for delay in delay_arr:
+                comment_txt = ''
+                comments = delay.fields.comment.comments
+                if len(comments) > 0:
+                    for comment in comments:
+                        if comment.created > '2024-09-02':
+                            comment_txt = comment_txt + comment.body
+                print(delay.permalink(), delay.get_field('summary'), '插入' if delay.get_field('created') > '2024-09-03'else '', comment_txt)
+            for inView in InView_arr:
+                comment_txt = ''
+                comments = inView.fields.comment.comments
+                if len(comments) > 0:
+                    for comment in comments:
+                        if comment.created > '2024-09-02':
+                            comment_txt = comment_txt + comment.body
+                print(inView.permalink(), inView.get_field('summary'), '插入' if inView.get_field('created') > '2024-09-03'else '', comment_txt)
+
+    print("总数", total, "插入", add_size, "完成", done_size, "延迟", delay_size, "延迟验证", InView_size )
+
+
+'''
+    # 创建子任务
+    all_story = find_all_story()
+    for story in all_story:
+        story_summary = story.get_field('summary')
+        if story_summary in ['泉州社保']:
+            task_assignee = story.get_field('assignee')     # 经办人
+            if task_assignee:
+                story_key = story.key
+                task_summary = "【" + story_summary + "】添加税务端填报基数流程"
+                task_description = "https://docs.qq.com/sheet/DSnhRV2lKUnVLb05G?tab=BB08J2"
+                task_component_id = '11015'  # 城市配置
+                add_sub_task(story_key, task_summary, task_description, task_assignee.name, task_component_id, fixVersions =version)
+'''
+
+'''
+    # 更新维护人
+    all_story = find_all_story()
+    for story in all_story:
+        summary = story.get_field('summary')
+        if summary == '广州社保':
+            story.update(fields={'customfield_11102': {'name': 'yanglianxin'}})
+        if summary == '珠海社保':
+            story.update(fields={'customfield_11102': {'name': 'yanglianxin'}})
+        if summary == '佛山社保':
+            story.update(fields={'customfield_11102': {'name': 'yanglianxin'}})
+        if summary == '江门社保':
+            story.update(fields={'customfield_11102': {'name': 'yanglianxin'}})
+        if summary == '湛江社保':
+            story.update(fields={'customfield_11102': {'name': 'yanglianxin'}})
+        if summary == '茂名社保':
+            story.update(fields={'customfield_11102': {'name': 'yanglianxin'}})
+        if summary == '肇庆社保':
+            story.update(fields={'customfield_11102': {'name': 'yanglianxin'}})
+        if summary == '惠州社保':
+            story.update(fields={'customfield_11102': {'name': 'yanglianxin'}})
+        if summary == '汕尾社保':
+            story.update(fields={'customfield_11102': {'name': 'yanglianxin'}})
+        if summary == '河源社保':
+            story.update(fields={'customfield_11102': {'name': 'yanglianxin'}})
+        if summary == '阳江社保':
+            story.update(fields={'customfield_11102': {'name': 'yanglianxin'}})
+        if summary == '清远社保':
+            story.update(fields={'customfield_11102': {'name': 'yanglianxin'}})
+        if summary == '东莞社保':
+            story.update(fields={'customfield_11102': {'name': 'yanglianxin'}})
+        if summary == '中山社保':
+            story.update(fields={'customfield_11102': {'name': 'yanglianxin'}})
+        if summary == '揭阳社保':
+            story.update(fields={'customfield_11102': {'name': 'yanglianxin'}})
+'''
+
+
+'''
+    # 查询修改记录
+    issues = jira.search_issues('project = "SEEBOT" AND fixVersion="{}"'.format(version), maxResults=1000, expand='changelog')
+    for issue in issues:
+        for history in issue.changelog.histories:
+            for item in history.items:
+                if item.field == 'Fix Version' and item.toString == 'BOT20240831':
+                    print(f"issue: {issue.key} ", issue.get_field('summary') , issue.get_field('assignee'))
+                    # print(f"\tField: {item.field}")
+                    # print(f"\tFrom: {item.fromString}")
+                    # print(f"\tTo: {item.toString}")
+                    # print(f"\t")
+'''
+
+'''
+    issues = jira.search_issues('project = "SEEBOT" AND issuetype="Test"', maxResults=1000)
+    for issue in issues:
+        summary = issue.get_field('summary')
+        if '在册流程,' in summary:
+            status = issue.get_field('status')
+            if status.name == '已完成':
+                print(summary.split('】')[0], status)
+'''
+
+'''
     # up_all_epic()
     all_story = find_all_story()
     for story in all_story:
         story_key = story.key
         summary = story.get_field('summary')
         if summary.endswith('社保') or summary.endswith('公积金'):
-            if summary == '广州公积金':
-                story.update(fields={'resolution': {"name": "未解决"}})
-            # task_summary = "【" + summary + "】费用明细流程, 确认该地区的客户正确跑出数据"
-            # task_description = "正确跑出该地区所有客户账户的费用数据，并在 参保管理 > 在户人数管理 > 费用明细, 可正确查询。"
-            # task_component_id = '11015'  # 城市配置
-            # # task_assignee = story.get_field('assignee').name
-            # task_assignee = story.get_field('customfield_11102')
-            # if task_assignee:
-            #     # print(story_key, story.get_field('summary'), story.permalink())
-            #     add_link_Test(story_key, task_summary, task_description, task_assignee.name, task_component_id)
+            # if summary.endswith('郑州社保'):
+            task_summary = "【" + summary + "】信息配置，标准化命名登录方式"
+            task_description = """要求：
+            1. 每个网站系统必须要配置登录方式，且统一命名字段名称和字段key为：
+            社保登录方式 socialLoginType
+            养老登录方式 ylLoginType
+            医疗登录方式 medicalLoginType
+            工伤登录方式 gsLoginType
+            单工伤登录方式 dgsLoginType
+            备案登录方式 baLoginType
+            税务登录方式 taxLoginType
+            金保登录方式 jbLoginType
+            失业登录方式 syLoginType
+            市网登录方式 swLoginType
+            公积金登录方式 accfundLoginType
 
+            2. 没有配置登录方式，需要按标准命名配置登录方式。
+
+            3. 有配置登录方式且与标准命名不一致，修改过程： （1) 加上新key。（2）所有的户（包括万科和仕邦所有客户）将旧key的值填写到新key上。（3）在信息配置中删除旧key（必须完成第（2）后）
+
+            4. 检查登录流程，步骤中原来使用旧key的，配置成新key
+
+            5. 登录方式下拉选项默认为五种：账号密码登录,CA证书登录,扫码登录,短信登录,接收器验证
+            如果登录方式中再有细分，需要加上前缀。比如网站有提供电子营业执照和APP2种扫码方式。扫码登录就要加上前缀，细分为：电子营业执照扫码登录、APP扫码登录
+
+            6. 登录方式下拉值更改后，需要确认所有户（包括万科和仕邦所有客户）的下拉选择正确。
+
+            """
+            subtasks = story.fields.subtasks
+            for subtask in subtasks:
+                if subtask.get_field('summary').endswith('【合肥社保】在册流程, 确认该地区的客户正确跑出数据'):
+                    changelog = jira.changelog(subtask)
+                    for history in changelog.histories:
+                        for item in history.items:
+                            print(f"\tField: {item.field}")
+                            print(f"\tFrom: {item.fromString}")
+                            print(f"\tTo: {item.toString}")
+                            print(f"\t")
+
+                #     subtask.update(description=task_description)
+
+
+            # task_component_id = '11015'  # 城市配置
+            # task_assignee = story.get_field('assignee')  # 经办人
+            # task_assignee = story.get_field('customfield_11102') # 维护人
+            # if task_assignee:
+            #     add_sub_task(story_key, task_summary, task_description, task_assignee.name, task_component_id)
+                # print(story_key, story.get_field('summary'), story.permalink())
+                # add_link_Test(story_key, task_summary, task_description, task_assignee.name, task_component_id)
+'''
 
     # find_story_no_task(version)
     # find_story_no_test(version, is_open=False)
