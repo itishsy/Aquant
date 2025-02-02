8d6e62b8b707d76aa2c30c1990d2659ccc04dfcd
itishsy <itishsy@163.com>
2025-02-02 10:34:08 +0800

    card

--- poker/strategies/sorted_hands.py
+++ poker/strategies/sorted_hands.py
@@ -1,51 +1,29 @@
-from treys import Card, Evaluator
-import itertools
-import random
-
-# 创建一副完整的牌
-suits = ['h', 'd', 'c', 's']
-ranks = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
-deck = [rank + suit for rank in ranks for suit in suits]
-
-# 生成所有可能的两张手牌组合
-all_hands = list(itertools.combinations(deck, 2))
-
-# 创建评估器
-evaluator = Evaluator()
-
-# 模拟次数
-num_simulations = 1000
-
-hand_win_rates = {}
-
-for hand in all_hands:
-    wins = 0
-    total = 0
-    hand_cards = [Card.new(card) for card in hand]
-
-    for _ in range(num_simulations):
-        remaining_deck = [Card.new(card) for card in deck if card not in hand]
-        random.shuffle(remaining_deck)
-        board = remaining_deck[:5]
-
-        score = evaluator.evaluate(board, hand_cards)
-
-        # 假设与随机对手比较
-        opponent_hand = remaining_deck[5:7]
-        opponent_score = evaluator.evaluate(board, opponent_hand)
-
-        if score < opponent_score:
-            wins += 1
-        total += 1
-
-    win_rate = wins / total
-    hand_win_rates[hand] = win_rate
-
-# 根据胜率对所有手牌进行排序
-sorted_hands = sorted(hand_win_rates.items(), key=lambda x: x[1], reverse=True)
-
-# 输出部分结果示例（前 10 个）
-for i, (hand, win_rate) in enumerate(sorted_hands[:2000], start=1):
-    print(f"排名 {i}: 手牌 {hand}, 胜率 {win_rate * 100:.2f}%")
-
-
+hands_win_rate = {
+    'AA': 88.88, 'KK': 85.52,
+    'QQ': 79.10, 'AKs': 78.60, 'JJ': 77.10, 'TT': 73.61, 'AQs': 71.88, 'AKo': 70.71,
+    'AJs': 66.70, 'AQo': 65.80, 'KQs': 63.81, 'ATs': 62.50, 'KJs': 60.90, '99': 60.52, '88': 60.31,
+    'AJo': 59.98, 'QJs': 59.88, 'KQo': 59.78, 'ATo': 59.68, 'KTs': 59.58, 'QTs': 59.48, 'JTs': 59.38,
+    '77': 58.60, '66': 58.20,
+    'KJo': 57.60, 'QJo': 56.50, '55': 56.30, '44': 56.20, 'KTo': 55.80,
+    'T9s': 55.60, 'QTo': 55.10, 'A9s': 53.80, 'A8s': 53.74, '33': 53.10, '22': 53.01,
+    'JTo': 52.80, 'J9s': 52.70, 'A7s': 52.50, 'A6s': 52.40,
+    'A5s': 51.80, 'A4s': 51.60, 'A3s': 51.30, 'A2s': 51.05,
+    '98s': 51.80, '97s': 51.70, '87s': 51.50, '86s': 51.40, '76s': 50.70,
+    '75s': 50.50, '65s': 50.40, '54s': 50.30, '43s': 50.20,
+    'A9o': 46.90, 'K9s': 46.10, 'A8o': 46.80, 'A7o': 46.70,
+    'A6o': 45.60, 'A5o': 45.50, 'A4o': 45.40, 'A3o': 45.30, 'A2o': 45.20,
+    'K9o': 41.10, 'K8s': 36.10, 'K8o': 35.10, 'K7o': 35.10, 'K7s': 36.10, 'K6s': 36.10, 'K6o': 35.10,
+    'K5o': 35.10, 'K5s': 35.10, 'K4s': 35.10, 'K4o': 35.10, 'K3s': 35.10, 'K3o': 35.10, 'K2o': 35.10, 'K2s': 35.10,
+    'Q9o': 41.10, 'Q9s': 42.10, 'Q8o': 35.10, 'Q8s': 39.10, 'Q7s': 35.10, 'Q7o': 35.10, 'Q6s': 35.10, 'Q6o': 35.10,
+    'Q5o': 35.10, 'Q5s': 42.10, 'Q4o': 35.10, 'Q4s': 35.10, 'Q3s': 35.10, 'Q3o': 35.10, 'Q2s': 35.10, 'Q2o': 35.10,
+    'J9o': 40.10, 'J8o': 35.10, 'J8s': 36.10, 'J7s': 36.10, 'J7o': 35.10, 'J6o': 35.10, 'J6s': 35.10, 'J5s': 35.10,
+    'J5o': 35.10, 'J4s': 35.10, 'J4o': 35.10, 'J3s': 35.10, 'J3o': 35.10, 'J2o': 35.10, 'J2s': 35.10, 'T9o': 40.10,
+    'T8o': 35.10, 'T8s': 39.10, 'T7s': 35.10, 'T7o': 35.10, 'T6o': 35.10, 'T6s': 35.10, 'T5s': 35.10, 'T5o': 35.10,
+    'T4o': 35.10, 'T4s': 35.10, 'T3o': 35.10, 'T3s': 35.10, 'T2s': 35.10, 'T2o': 35.10, '98o': 36.10, '97o': 35.10,
+    '96s': 36.10, '96o': 35.10, '95s': 35.10, '95o': 35.10, '94o': 35.10, '94s': 35.10, '93o': 35.10, '93s': 35.10,
+    '92s': 35.10, '92o': 35.10, '87o': 35.10, '86o': 35.10, '85s': 35.10, '85o': 35.10, '84o': 35.10, '84s': 35.10,
+    '83s': 35.10, '83o': 35.10, '82o': 35.10, '82s': 35.10, '76o': 35.10, '75o': 35.10, '74s': 35.10, '74o': 35.10,
+    '73o': 35.10, '73s': 35.10, '72s': 35.10, '72o': 35.10, '65o': 35.10, '64o': 35.10, '64s': 35.10, '63s': 35.10,
+    '63o': 35.10, '62o': 35.10, '62s': 35.10, '54o': 35.10, '53o': 35.10, '53s': 35.10, '52o': 35.10, '52s': 35.10,
+    '43o': 35.10, '42o': 35.10, '42s': 35.10, '32o': 35.10, '32s': 35.10,
+}
